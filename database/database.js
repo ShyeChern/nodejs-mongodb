551f1b2a546620cb61ceaf9fcc74867471bf8a11
ShyeChern <chern-97@hotmail.com>
2022-11-27 21:31:14 +0800

    update db init

--- database/database.js
+++ database/database.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 // initialize database
-module.exports.init = async () => {
-	try {
-		const database = await mongoose.connect(process.env.MONGODB_URL);
+module.exports.init = () => {
+	mongoose.connect(process.env.MONGODB_URL, (err) => {
+		if (err) {
+			console.log('Fail to connect database. ' + err.message);
+			return;
+		}
 		console.log('Connected to database');
-		return database;
-	} catch (e) {
-		console.log('Fail to connect database. ' + e.message);
-	}
+	});
 };
 
-// for transaction query
+// db connection - for transaction query
 module.exports.conn = mongoose.createConnection(process.env.MONGODB_URL);
